66cd5f280e950acdd23380689e7e3fae47569834
Prashantsingh-8 <82604415+Prashantsingh-8@users.noreply.github.com>
2022-01-12 14:34:24 +0530

    .

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright Prashant Singh</p>
+    </footer>
+  );
+}
+
+export default Footer;
